48535884272479fc9d1b5d16d8b8ef747092be67
Ibrahim Hussain <ibrahim.hucyn@live.com>
2019-08-10 03:00:43 +0500

    Design completed

--- SwatInc.PopUp/PopUpUI.Designer.vb
+++ SwatInc.PopUp/PopUpUI.Designer.vb
@@ -19,13 +19,70 @@ Partial Class PopUpUI
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.PictureBoxPopUpIcon = New System.Windows.Forms.PictureBox()
+        Me.LabelControlHeading = New DevExpress.XtraEditors.LabelControl()
+        Me.LabelControlMessage = New DevExpress.XtraEditors.LabelControl()
+        CType(Me.PictureBoxPopUpIcon, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'PictureBoxPopUpIcon
+        '
+        Me.PictureBoxPopUpIcon.Image = Global.SwatInc.PopUp.My.Resources.Resources.HealthForms
+        Me.PictureBoxPopUpIcon.Location = New System.Drawing.Point(12, 0)
+        Me.PictureBoxPopUpIcon.Name = "PictureBoxPopUpIcon"
+        Me.PictureBoxPopUpIcon.Size = New System.Drawing.Size(96, 96)
+        Me.PictureBoxPopUpIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBoxPopUpIcon.TabIndex = 0
+        Me.PictureBoxPopUpIcon.TabStop = False
+        '
+        'LabelControlHeading
+        '
+        Me.LabelControlHeading.Appearance.Font = New System.Drawing.Font("Comic Sans MS", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelControlHeading.Appearance.FontStyleDelta = System.Drawing.FontStyle.Bold
+        Me.LabelControlHeading.Appearance.Options.UseFont = True
+        Me.LabelControlHeading.AppearanceDisabled.Font = New System.Drawing.Font("Comic Sans MS", 12.0!, System.Drawing.FontStyle.Bold)
+        Me.LabelControlHeading.AppearanceDisabled.FontStyleDelta = System.Drawing.FontStyle.Bold
+        Me.LabelControlHeading.AppearanceDisabled.Options.UseFont = True
+        Me.LabelControlHeading.AppearanceHovered.Font = New System.Drawing.Font("Comic Sans MS", 12.0!, System.Drawing.FontStyle.Bold)
+        Me.LabelControlHeading.AppearanceHovered.FontStyleDelta = System.Drawing.FontStyle.Bold
+        Me.LabelControlHeading.AppearanceHovered.Options.UseFont = True
+        Me.LabelControlHeading.AppearancePressed.Font = New System.Drawing.Font("Comic Sans MS", 12.0!, System.Drawing.FontStyle.Bold)
+        Me.LabelControlHeading.AppearancePressed.FontStyleDelta = System.Drawing.FontStyle.Bold
+        Me.LabelControlHeading.AppearancePressed.Options.UseFont = True
+        Me.LabelControlHeading.Location = New System.Drawing.Point(122, 0)
+        Me.LabelControlHeading.Name = "LabelControlHeading"
+        Me.LabelControlHeading.Size = New System.Drawing.Size(60, 23)
+        Me.LabelControlHeading.TabIndex = 1
+        Me.LabelControlHeading.Text = "Heading"
+        '
+        'LabelControlMessage
+        '
+        Me.LabelControlMessage.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.LabelControlMessage.Appearance.Options.UseTextOptions = True
+        Me.LabelControlMessage.Appearance.TextOptions.VAlignment = DevExpress.Utils.VertAlignment.Top
+        Me.LabelControlMessage.AppearanceDisabled.Options.UseTextOptions = True
+        Me.LabelControlMessage.AppearanceDisabled.TextOptions.VAlignment = DevExpress.Utils.VertAlignment.Top
+        Me.LabelControlMessage.AppearanceHovered.Options.UseTextOptions = True
+        Me.LabelControlMessage.AppearanceHovered.TextOptions.VAlignment = DevExpress.Utils.VertAlignment.Top
+        Me.LabelControlMessage.AppearancePressed.Options.UseTextOptions = True
+        Me.LabelControlMessage.AppearancePressed.TextOptions.VAlignment = DevExpress.Utils.VertAlignment.Top
+        Me.LabelControlMessage.AutoSizeMode = DevExpress.XtraEditors.LabelAutoSizeMode.None
+        Me.LabelControlMessage.ImageAlignToText = DevExpress.XtraEditors.ImageAlignToText.LeftTop
+        Me.LabelControlMessage.Location = New System.Drawing.Point(122, 29)
+        Me.LabelControlMessage.Name = "LabelControlMessage"
+        Me.LabelControlMessage.Size = New System.Drawing.Size(343, 67)
+        Me.LabelControlMessage.TabIndex = 2
+        Me.LabelControlMessage.Text = "Message displayed on PopUpUI"
         '
         'PopUpUI
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(477, 122)
+        Me.ClientSize = New System.Drawing.Size(477, 100)
+        Me.Controls.Add(Me.LabelControlMessage)
+        Me.Controls.Add(Me.LabelControlHeading)
+        Me.Controls.Add(Me.PictureBoxPopUpIcon)
         Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.MaximizeBox = False
         Me.MinimizeBox = False
@@ -33,7 +90,13 @@ Partial Class PopUpUI
         Me.ShowIcon = False
         Me.ShowInTaskbar = False
         Me.Text = "PopUpUI"
+        CType(Me.PictureBoxPopUpIcon, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+
+    Friend WithEvents PictureBoxPopUpIcon As PictureBox
+    Friend WithEvents LabelControlHeading As DevExpress.XtraEditors.LabelControl
+    Friend WithEvents LabelControlMessage As DevExpress.XtraEditors.LabelControl
 End Class
--- SwatInc.PopUp/PopUpUI.vb
+++ SwatInc.PopUp/PopUpUI.vb
@@ -1,3 +1,2 @@
 ﻿Public Class PopUpUI
-
 End Class
