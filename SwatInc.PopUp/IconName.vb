126e21232e47a2051644be6410261f7fdc8788c6
Ibrahim Hussain <ibrahim.hucyn@live.com>
2019-08-10 02:07:42 +0500

    Add project files.

---
+++ SwatInc.PopUp/IconName.vb
@@ -0,0 +1,20 @@
+﻿
+
+Public Enum IconName
+    SocketDisconnected
+    Successful
+    Authentication
+    PatientProfile
+    ClinicalChemistry
+    Haematology
+    Logout
+    Patient
+    Settings
+    HealthForms
+    IDCard
+    Exception
+    RecordsAdded
+    SwatIncNinja
+    NonExistentImageForTesting
+    NoRecord
+End Enum
---
+++ SwatInc.PopUp/PopUpEventArgs.vb
@@ -0,0 +1,19 @@
+﻿Public Class PopUpEventArgs
+    Inherits EventArgs
+    Public Property Message As String
+    Public Property Title As String
+    Public WriteOnly Property PngIconRefName As IconName
+        Set
+            _pngIconName = Value.ToString
+        End Set
+    End Property
+
+    Private _pngIconName As String
+    Public ReadOnly Property PngIconName As String
+        Get
+            Return _pngIconName
+        End Get
+    End Property
+
+    Public Property Heading As String
+End Class
